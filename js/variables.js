ae8a87109e1aae92f368d1932b9e6aa23afb0d79
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-01-03 09:12:52 +0800

    variables.js

---
+++ js/variables.js
@@ -0,0 +1,22 @@
+    // Declaring different variables of different data types
+
+    let firstName = 'Jemuel'   
+    let lastName = 'Repoylo' 
+    let country = 'Philippines'
+    let city = "Manila"
+    let age = '22'
+    let isMarried = false
+
+    // Declaring variables with number values
+
+    const gravity = 9.81        // Earth Gravity in m/s2
+    const boilingPoint = 100    // Water boiling point in degC
+    const PI = 3.14             // Geometrical Constant
+
+    // Variable can also be declared in one line separating by a comma
+
+    let name = 'JemSy',
+        job = 'Developer'
+        live = 'PH'
+
+
